aace94580907ee3052f4bb39bfb884bdf8c9fc59
David Englert <mail@davidenglert.de>
2025-09-13 22:12:53 +0200

    add origin correct

--- api/main.py
+++ api/main.py
@@ -57,7 +57,7 @@ app.add_middleware(SentryAsgiMiddleware)
 app.add_middleware(
     CORSMiddleware,
     #allow_origins=["https://api.vatifytax.app", "https://localhost:5173"],
-    allow_origins=["https://api.vatifytax.app"],
+    allow_origins=["https://dashboard.vatifytax.app"],
     allow_credentials=True,            # wichtig für Cookies
     allow_methods=["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
     allow_headers=["Content-Type","Authorization","X-Requested-With","x-csrf-token"]
